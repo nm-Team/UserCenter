ac6b1434acd919e1810ea67c79e2c319b2141391
Yuinars <i@yuina.art>
2021-06-26 01:29:21 +0900

    修复问题

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,37 @@
-const apiURL = "http://api.nmteam.ml/";
-
-async function getJSON(url) {
-    try {
-        let response = await fetch(url, {
-            method: 'POST',
-            mode: 'no-cors',
-        });
-        return await response.text();
-    } catch (error) {
-        console.log('Request Failed', error);
-        info.innerHTML = "<t data-i18n='errorNet'></t>" + error;
-        changeLanguage();
-        return -1;
-    }
-}
+var apiURL = "https://nmapi.c0y.cc/";
 
 function login() {
+    let user = uname.value;
+    let passwd = pass.value;
+$.ajax(apiURL+"login.php", {
+type:"POST",
+async:false,
+data:{ "user":user, "pass":passwd},
+crossDomain:true,
+datatype: "jsonp",
+xhrFields: {withCredentials: true},
+success:function(data){
+    let status = data['status'];
+    if (status == "successful") {
+        info.innerHTML = "<t>OK</t>";
+    }else if (status == "error") {
+        info.innerHTML = "<t data-i18n='errorOccured'></t>" + data['info'];
+    }
+    changeLanguage();
+
+},
+error:function(){
+info.innerHTML = "<t data-i18n='errorOccured'></t>" + data['info']}
+});
+    
+}
+
+function login_old() {
     if (uname.value && pass.value) {
-        json = load(apiURL + "login.php?name=" + uname.value + "&pass=" + pass.value + "&t=" + time());
+        json = load(apiURL + "login.php?user=" + uname.value + "&pass=" + pass.value + "&t=" + time());
     }
     returnJSON = JSON.parse(json);
     if (returnJSON.status == "successful") {
-        // 登录成功，然后呢
     }
     else if (returnJSON.status == "error") {
         info.innerHTML = "<t data-i18n='errorOccured'></t>" + returnJSON.info;
@@ -32,13 +42,4 @@ function login() {
 function time() {
     timeNow = new Date;
     return timeNow.getTime();
-}
-
-function load(url, fun = false) {
-    // 如果有好心人，请帮我用异步或者fetch什么的重写一下，我不是这块料，自裁了
-    var xmlhttp;
-    xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("POST", url, false);
-    xmlhttp.send();
-    return xmlhttp.responseText;
 }
