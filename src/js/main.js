b0aa422ec3936b80f9786c0c6179c462cae5b989
agou <2833769658@qq.com>
2021-06-26 13:38:35 +0800

    完成登录和注册功能~

--- src/js/main.js
+++ src/js/main.js
@@ -1,49 +1,104 @@
-var apiURL = "https://nmapi.c0y.cc/";
+var apiURL = "";
+
+if (getQueryVariable("name")) {
+    changeLoginTypeA.setAttribute("href", changeLoginTypeA.getAttribute("href") + "?returnto=" + getQueryVariable("returnto") + "&name=" + getQueryVariable("name"));
+    nameBox.innerHTML = "<t data-i18n='toContinue'></t> " + getQueryVariable("name");
+}
 
 function login() {
     let user = uname.value;
     let passwd = pass.value;
-if(user.length > 4 && user.length<16 )
-{info.innerHTML = "<t>username too long/short.</t>";}
-else if(passwd.length < 4)
-{info.innerHTML = "<t>password too short.</t>";}else{
-    $.ajax(apiURL+"login.php", {
-type:"POST",
-async:false,
-data:{ "user":user, "pass":passwd},
-crossDomain:true,
-datatype: "jsonp",
-xhrFields: {withCredentials: true},
-success:function(data){
-    let status = data['status'];
-    if (status == "successful") {
-        info.innerHTML = "<t>OK</t>";
-    }else if (status == "error") {
-        info.innerHTML = "<t data-i18n='errorOccured'></t>" + data['info'];
+    if (!(user && passwd)) {
+        info.innerHTML = "<t data-i18n='errorOccured'></t>" + "<t data-i18n='fillall'></t>";
+    }
+    if ((user.length < 4 && user.length > 16) || passwd.length < 4) { info.innerHTML = "<t data-i18n='errorOccured'></t><tdata-i18n='lengthError'></t>"; }
+    else {
+        info.innerHTML = "<t data-i18n='loging'></t>";
+        $.ajax(apiURL + "login.php", {
+            type: "POST",
+            async: false,
+            data: { "user": user, "pass": passwd, "g-recaptcha-response": grecaptcha.getResponse() },
+            crossDomain: true,
+            datatype: "jsonp",
+            xhrFields: { withCredentials: true },
+            success: function (data) {
+                let status = data['status'];
+                if (status == "successful") {
+                    info.innerHTML = "<t data-i18n='logok'></t>";
+                    returnURL = getQueryVariable("returnto");
+                    if (!returnURL) returnURL = "/info.html";
+                    window.location.href = returnURL;
+                } else if (status == "error") {
+                    info.innerHTML = "<t data-i18n='errorOccured'></t>" + data['info'];
+                }
+
+            },
+            error: function () {
+                info.innerHTML = "<t data-i18n='errorOccured'></t><t data-i18n='errorNet'></t>" + data['info'];
+            }
+        });
     }
     changeLanguage();
-
-},
-error:function(){
-info.innerHTML = "<t data-i18n='errorOccured'></t>" + data['info']}
-});
-}
+    grecaptcha.reset();
 }
 
-function login_old() {
-    if (uname.value && pass.value) {
-        json = load(apiURL + "login.php?user=" + uname.value + "&pass=" + pass.value + "&t=" + time());
+function register() {
+    let user = uname.value;
+    let emailadd = email.value;
+    let passwd = pass.value;
+    let passwd2 = pass2.value;
+    if (!(user && emailadd && passwd && passwd2)) {
+        info.innerHTML = "<t data-i18n='errorOccured'></t>" + "<t data-i18n='fillall'></t>";
     }
-    returnJSON = JSON.parse(json);
-    if (returnJSON.status == "successful") {
+    else if (!readNeeds.checked) {
+        info.innerHTML = "<t data-i18n='errorOccured'></t>" + "<t data-i18n='checkterms'></t>";
     }
-    else if (returnJSON.status == "error") {
-        info.innerHTML = "<t data-i18n='errorOccured'></t>" + returnJSON.info;
+    else if ((user.length < 4 && user.length > 16) || passwd.length < 4) { info.innerHTML = "<tdata-i18n='lengthError'></t>"; }
+    else if (passwd != passwd2) {
+        info.innerHTML = "<t data-i18n='errorOccured'></t>" + "<t data-i18n='2timepwnotsame'></t>";
+        pass.value=pass2.value="";
+
+    }
+    else if ((user.length < 4 && user.length > 16) || passwd.length < 4) { info.innerHTML = "<t data-i18n='errorOccured'></t><tdata-i18n='lengthError'></t>"; }
+    else {
+        info.innerHTML = "<t data-i18n='registering'></t>";
+        $.ajax(apiURL + "register.php", {
+            type: "POST",
+            async: false,
+            data: { "user": user, "email": emailadd, "pass": passwd, "g-recaptcha-response": grecaptcha.getResponse() },
+            crossDomain: true,
+            datatype: "jsonp",
+            xhrFields: { withCredentials: true },
+            success: function (data) {
+                let status = data['status'];
+                if (status == "successful") {
+                    info.innerHTML = "<t data-i18n='registerok'></t>";
+                    window.location.href = changeLoginTypeA.getAttribute("href") ;
+                } else if (status == "error") {
+                    info.innerHTML = "<t data-i18n='errorOccured'></t>" + data['info'];
+                }
+
+            },
+            error: function () {
+                info.innerHTML = "<t data-i18n='errorOccured'></t><t data-i18n='errorNet'></t>" + data['info']
+            }
+        });
     }
     changeLanguage();
+    grecaptcha.reset();
 }
 
 function time() {
     timeNow = new Date;
     return timeNow.getTime();
+}
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) { return pair[1]; }
+    }
+    return null;
 }
